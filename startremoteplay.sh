92260fcb241a005c935079b0bd82542d4a14802d
snipem <mail@matthias-kuech.de>
2015-08-30 17:07:43 +0200

    Init commit
    With example for instructions

---
+++ startremoteplay.sh
@@ -0,0 +1,5 @@
+adb shell am start -n com.playstation.remoteplay/.RpActivityMain
+sleep 5
+adb shell input tap 850 460
+source Xperia_Z2_Tablet/coords_portrait
+#source Xperia_Z2_Tablet/coords_landscape
